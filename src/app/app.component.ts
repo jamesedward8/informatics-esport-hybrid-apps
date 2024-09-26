59c307078496f9e4d871b663ce00dbc19a6729ff
mathewrs04 <164150651+mathewrs04@users.noreply.github.com>
2024-09-26 17:47:58 +0700

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) {
+    // Cek URL yang aktif setiap kali navigasi berubah
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Tentukan apakah URL aktif adalah 'home'
+        this.isHomePage = event.url === '/home' || event.urlAfterRedirects === '/home';
+      }
+    });
+  }
+ 
 }
